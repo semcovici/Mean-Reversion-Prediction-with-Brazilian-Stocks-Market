1bcd9eaf890f61ebd856283385f9e40969711f8f
semcovici <pedrosemcovici@outlook.com>
2024-04-12 18:53:03 -0300

    adiciona ativos certos

--- src/data/get_data_yfinance.py
+++ src/data/get_data_yfinance.py
@@ -1,11 +1,11 @@
 import yfinance as yahooFinance
 
 
-list_ativos =["PETR4.SA","PRIO3.SA", "VALE", "GGBR3.SA"]
+list_ativos =["PETR4.SA","PRIO3.SA", "VALE3.SA", "GGBR3.SA", "ABCB4.SA", "ITUB3.SA", "FLRY3.SA", "RADL3.SA"]
 start_dt="1910-01-01" 
 end_dt="2100-12-31"
 
-path_price_history = '../../data/raw/price_history_{ativo}.xlsx'
+path_price_history = 'data/raw/price_history_{ativo}.xlsx'
 
 
 if __name__ == '__main__':
