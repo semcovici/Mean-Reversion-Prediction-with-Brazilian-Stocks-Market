33fa3656da3055af04bc082cb4d0f96e8a5ae81e
semcovici <pedrosemcovici@outlook.com>
2024-05-12 13:17:22 -0300

    feat: independent contigency tables

--- src/features/contigency_table.py
+++ src/features/contigency_table.py
@@ -1,19 +1,51 @@
 import pandas as pd
 from tqdm import tqdm
+
+def process_row_contigency_table(row, data, n_prev_meta, return_only_n_prev_meta):
+    
+    meta = row['meta']
+    date = row['Date']
+    idx = row.index
+    
+    if idx < n_prev_meta:
+        return None 
+    
+    
+    new_row = {
+        'Date': date,
+        'Meta': meta
+    }
+    
+    if return_only_n_prev_meta:
+        
+        new_row.update({f'Meta {- n_prev_meta}': data.loc[idx -(n_prev_meta),'meta']})
+    else:
+        new_row.update({f'Meta {-(meta + 1)}': data.loc[idx -(meta + 1),'meta'] for meta in range(n_prev_meta)})
+        
+    
+    return pd.Series(new_row)
+
+
+
 def create_contigency_table(
     data,
     n_prev_meta,
     progress_bar=True,
-    as_probability=False
+    as_probability=False,
+    return_only_n_prev_meta = False
 ):
-
+    # reset index (this code do not work without a sequential index)
     data.reset_index(drop = True, inplace=True)
-
-    table_schema = {'Date': [],'Meta':[]}
-    table_schema.update({f'Meta {-(meta + 1)}': [] for meta in range(n_prev_meta)})
-
+    
+    if return_only_n_prev_meta:
+        table_schema = {'Date': [],'Meta':[], f'Meta {- n_prev_meta}': []}
+    else:
+        # create table
+        table_schema = {'Date': [],'Meta':[]}
+        table_schema.update({f'Meta {-(meta + 1)}': [] for meta in range(n_prev_meta)})
+        
     df_prev_meta = pd.DataFrame(table_schema)
-
+            
     for idx, row in tqdm(data.iterrows(), total = len(data), disable=not progress_bar):
 
         meta = row['meta']
@@ -22,20 +54,35 @@ def create_contigency_table(
         if idx < n_prev_meta:
             continue
         
+        
         new_row = {
             'Date': date,
             'Meta': meta
         }
         
-        new_row.update({f'Meta {-(meta + 1)}': data.loc[idx -(meta + 1),'meta'] for meta in range(n_prev_meta)})
+        if return_only_n_prev_meta:
+            
+            new_row.update({f'Meta {- n_prev_meta}': data.loc[idx -(n_prev_meta),'meta']})
+        else:
+            new_row.update({f'Meta {-(meta + 1)}': data.loc[idx -(meta + 1),'meta'] for meta in range(n_prev_meta)})
+            
         
         df_prev_meta.loc[len(df_prev_meta)] = new_row
         
-    cont_tbl = pd.crosstab(
-    df_prev_meta['Meta'],
-    [df_prev_meta[f'Meta {-(meta + 1)}'] for meta in range(n_prev_meta)],
-    margins=False
-    )
+        
+    if return_only_n_prev_meta:
+        cont_tbl = pd.crosstab(
+        df_prev_meta['Meta'],
+        [df_prev_meta[f'Meta {- n_prev_meta}']],
+        margins=False
+        )  
+        
+    else:
+        cont_tbl = pd.crosstab(
+        df_prev_meta['Meta'],
+        [df_prev_meta[f'Meta {-(meta + 1)}'] for meta in range(n_prev_meta)],
+        margins=False
+        )
     
     if as_probability:        
         cont_tbl_probas = cont_tbl.apply(lambda x: [col/sum(x) for col in x])    
--- src/features/create_contigency_table_meta.py
+++ src/features/create_contigency_table_meta.py
@@ -7,6 +7,8 @@ import plotly.graph_objects as go
 import pandas as pd
 from tqdm import tqdm
 
+
+from contigency_table import create_contigency_table
 # ### Definitions
 path_data_dir = 'data/'
 
@@ -17,27 +19,48 @@ relevant_cols = ['Date', 'Close', 'Volume']
 
 def main():
     
-    for n_prev_meta in range(1, 21):
+    for n_prev_meta in range(1, 6):
         
-        for asset in tqdm(list_assets, desc=f'{n_prev_meta} of {10}'):
+        for asset in tqdm(list_assets, desc=f'{n_prev_meta} of {5}'):
             
-            # create for full dataset
+            
             data = pd.read_csv(path_data_dir + f"processed/price_history_{asset.replace('.', '_')}_meta_dataset_ffill.csv")
             
-            cont_tbl = create_contigency_table(data,n_prev_meta, progress_bar=False)
-            
-            cont_tbl.to_csv(path_data_dir + f"processed/contingency_table_price_history_{asset.replace('.', '_')}_meta_range({n_prev_meta})_dataset_ffill.csv")
-            
-            # create for train test
             train = pd.read_csv(path_data_dir + f"processed/train_price_history_{asset.replace('.', '_')}_meta_dataset_ffill.csv")
             test = pd.read_csv(path_data_dir + f"processed/test_price_history_{asset.replace('.', '_')}_meta_dataset_ffill.csv")
             
-            cont_tbl_train = create_contigency_table(train,n_prev_meta, progress_bar=False)
-            cont_tbl_test = create_contigency_table(test,n_prev_meta, progress_bar=False)
+            ######################
+            # dependent
+            ######################
             
-            cont_tbl_train.to_csv(path_data_dir + f"processed/train_contingency_table_price_history_{asset.replace('.', '_')}_meta_range({n_prev_meta})_dataset_ffill.csv")
-            cont_tbl_test.to_csv(path_data_dir + f"processed/test_contingency_table_price_history_{asset.replace('.', '_')}_meta_range({n_prev_meta})_dataset_ffill.csv")
+            # # create for full dataset
+            # cont_tbl = create_contigency_table(data,n_prev_meta, progress_bar=False)
+            
+            # cont_tbl.to_csv(path_data_dir + f"processed/contingency_table_price_history_{asset.replace('.', '_')}_meta_range({n_prev_meta})_dataset_ffill.csv")
+            
+            # # create for train test
+            # cont_tbl_train = create_contigency_table(train,n_prev_meta, progress_bar=False)
+            # cont_tbl_test = create_contigency_table(test,n_prev_meta, progress_bar=False)
+            
+            # cont_tbl_train.to_csv(path_data_dir + f"processed/train_contingency_table_price_history_{asset.replace('.', '_')}_meta_range({n_prev_meta})_dataset_ffill.csv")
+            # cont_tbl_test.to_csv(path_data_dir + f"processed/test_contingency_table_price_history_{asset.replace('.', '_')}_meta_range({n_prev_meta})_dataset_ffill.csv")
 
+
+            ######################
+            # independent
+            ######################
+            
+            # create for full dataset
+            cont_tbl = create_contigency_table(data,n_prev_meta, progress_bar=False, return_only_n_prev_meta=True)
+            
+            cont_tbl.to_csv(path_data_dir + f"processed/contingency_table_ind_price_history_{asset.replace('.', '_')}_meta_-{n_prev_meta}_dataset_ffill.csv")
+            
+            # create for train test
+            cont_tbl_train = create_contigency_table(train,n_prev_meta, progress_bar=False, return_only_n_prev_meta=True)
+            cont_tbl_test = create_contigency_table(test,n_prev_meta, progress_bar=False, return_only_n_prev_meta=True)
+            
+            cont_tbl_train.to_csv(path_data_dir + f"processed/train_contingency_table_ind_price_history_{asset.replace('.', '_')}_meta_-{n_prev_meta}_dataset_ffill.csv")
+            cont_tbl_test.to_csv(path_data_dir + f"processed/test_contingency_table_ind_price_history_{asset.replace('.', '_')}_meta_-{n_prev_meta}_dataset_ffill.csv")
 
 if __name__ == '__main__':
     
